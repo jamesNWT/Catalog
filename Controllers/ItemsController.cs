bf10e65c51d65608c1d4c0d543ce6f20fc47edbe
James Wilcox <jameswilcox.98@gmail.com>
2021-10-12 11:51:40 -0300

    kubernetes part done

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using Catalog.Dtos;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.Controllers
 {
@@ -14,10 +15,11 @@ namespace Catalog.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IItemsRepository repository;
-
-        public ItemsController(IItemsRepository repository)
+        private readonly ILogger<ItemsController> logger;
+        public ItemsController(IItemsRepository repository, ILogger<ItemsController> logger)
         {
             this.repository = repository;
+            this.logger = logger;
         }
 
         [HttpGet]
@@ -25,6 +27,8 @@ namespace Catalog.Controllers
         {
             var items = (await repository.GetItemsAsync())
                         .Select(item => item.AsDto());
+
+            logger.LogInformation($"{DateTime.UtcNow.ToString("hh:mm:ss")}: Retrieved {items.Count()} items");
 
             return items;
         }
